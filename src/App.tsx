a10eefaa1e708e8931c13943bca4256efd2811ec
Simon LUCIDO <simon.lucido@etu.umontpellier.fr>
2023-04-28 17:21:16 +0200

    feat: api service for orders & products
    Signed-off-by: Simon LUCIDO <simon.lucido@etu.umontpellier.fr>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import { apiService } from './services/APIService';
+
 function App() {
+  apiService.getProducts().then(console.log);
+  apiService.getOrders().then(console.log);
+
   return (
     <div>
       <h1>Polystore</h1>
---
+++ src/services/APIService.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosResponse } from 'axios';
+import { apiConfig } from '../config/apiConfig';
+import { ProductAggregateDTO } from '../models/productAggregateDTO';
+import { OrderDTO } from '../models/orderDTO';
+
+class APIService {
+  private api = axios.create({
+    baseURL: apiConfig.baseURL,
+  });
+
+  async getProducts(): Promise<ProductAggregateDTO[]> {
+    const response: AxiosResponse<ProductAggregateDTO[]> = await this.api.get('/api/v1/products');
+    return response.data;
+  }
+
+  async getProduct(id: string): Promise<ProductAggregateDTO> {
+    const response: AxiosResponse<ProductAggregateDTO> = await this.api.get(
+      `/api/v1/products/${id}`,
+    );
+    return response.data;
+  }
+
+  async getOrders(): Promise<OrderDTO[]> {
+    const response: AxiosResponse<OrderDTO[]> = await this.api.get('/api/v1/orders');
+    return response.data;
+  }
+
+  async getOrder(id: string): Promise<OrderDTO> {
+    const response: AxiosResponse<OrderDTO> = await this.api.get(`/api/v1/orders/${id}`);
+    return response.data;
+  }
+}
+
+export const apiService = new APIService();
