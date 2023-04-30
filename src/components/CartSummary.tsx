cc84f7b19bbb6477311a2d9d7079b3085182d5e4
Simon LUCIDO <simon.lucido@etu.umontpellier.fr>
2023-04-30 13:19:19 +0200

    feat: add cart summary component
    Signed-off-by: Simon LUCIDO <simon.lucido@etu.umontpellier.fr>

---
+++ src/components/CartSummary.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { useCart } from '../context/CartContext';
+import { ProductAggregateDTO } from '../models/productAggregateDTO';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const CartSummary: React.FC = () => {
+  const { items, removeFromCart } = useCart();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const totalItems = items.reduce((total, item) => total + item.quantity, 0);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOrder = () => {
+    // Handle order creation here
+  };
+
+  return (
+    <div className="relative">
+      <button className="text-white px-2 py-1 rounded bg-blue-500" onClick={toggleMenu}>
+        <FontAwesomeIcon icon={faShoppingCart} />({totalItems})
+      </button>
+      {isOpen && (
+        <div className="absolute right-0 mt-2 w-64 bg-white shadow-md rounded border p-4">
+          {items.map((item: ProductAggregateDTO & { quantity: number }) => (
+            <div key={item.id} className="flex justify-between items-center mb-2">
+              <span>
+                {item.name} x{item.quantity}
+              </span>
+              <button className="text-red-500" onClick={() => removeFromCart(item.id)}>
+                Remove
+              </button>
+            </div>
+          ))}
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded mt-4 w-full"
+            onClick={handleOrder}
+          >
+            Order
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CartSummary;
